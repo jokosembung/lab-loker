c1937ab7152ca3d7d37cdf5437efaa3cf42692e0
imam.m <imam.mhm21@gmail.com>
2025-05-06 16:03:27 +0700

    fixing

--- functions/member/bio/update.php
+++ functions/member/bio/update.php
@@ -9,10 +9,11 @@ $about = $_POST["about"];
 $userID = $user['id'];
 $about = $conn->real_escape_string(strip_tags($about));
 
-$sql = "UPDATE users set about = '$about' WHERE id = $userID";
+$sql = "UPDATE users set about = ? WHERE id = ?";
+$stmtUpdate = $conn->prepare($sql);
+$stmtUpdate->bind_param("si", $about, $userID);
 
-
-if ($conn->query($sql) === FALSE) {
+if (!$stmtUpdate->execute()) {
     $_SESSION['error_message'] = "Error: " . $sql . "<br>" . $conn->error;
     echo "<script>window.location.href = '{$menuProfileEditBio}';</script>";
     exit;
